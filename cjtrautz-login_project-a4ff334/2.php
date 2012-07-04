ab825a4751248d41585da5ff310116317deca966
colton <cjtrautz@gmail.com>
2012-07-04 14:40:08 -0700

    Reverted conflict commit

---
+++ cjtrautz-login_project-a4ff334/2.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Checks level 2. Echo access level
+ *
+ * @author Colton Trautz <colton@sendpepper.com>
+ *
+ * 
+ */
+ 
+session_start();
+
+include_once 'includes/restriction.php';
+
+check_level(2);
